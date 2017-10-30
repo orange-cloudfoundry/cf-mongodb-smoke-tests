86f5ae32f9e05adcc9a6eed675da9ec6316273de
David Dieng <david.dieng@orange.com>
2017-10-30 17:14:00 +0100

    Add the mgo dependency + Write some basic code to use it

--- readwrite/readwrite_test.go
+++ readwrite/readwrite_test.go
@@ -1,6 +1,7 @@
 package readwrite_test
 
 import (
+	"gopkg.in/mgo.v2"
 
 	. "github.com/onsi/ginkgo"
 	// . "github.com/onsi/gomega"
@@ -12,7 +13,11 @@ var _ = Describe("MongoDB Service", func() {
 
 	Context("given a standalone MongoDB server", func() {
 		It("connects as user, creates collection, writes some data in collection, read the data back, updates the data, deletes data, verify data is absent", func() {
-			// ...
+			session, err := mgo.Dial("127.0.0.1:27017")
+			if err != nil {
+				panic(err)
+			}
+			defer session.Close()
 		})
 	})
 })
