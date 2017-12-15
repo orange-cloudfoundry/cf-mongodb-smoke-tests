ff0e835a23d0095c8faf7b39fe38bbf422d389f0
Benjamin Gandon <bgandon.ext@orange.com>
2017-12-15 19:10:36 +0100

    Use 'gofmt -w' to format code

--- readwrite/readwrite_test.go
+++ readwrite/readwrite_test.go
@@ -1,109 +1,111 @@
 package readwrite_test
 
 import (
-	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
-var randUser, randPwd, randDb, randCol  = "Username","Password", "Database", "Collection"
+var randUser, randPwd, randDb, randCol = "Username", "Password", "Database", "Collection"
 var user = mgo.User{
-		Username: randUser,
-		Password: randPwd,
-		Roles: []mgo.Role{ mgo.RoleDBAdmin },
+	Username: randUser,
+	Password: randPwd,
+	Roles:    []mgo.Role{mgo.RoleDBAdmin},
 }
+
 type Person struct {
-		Id    bson.ObjectId   "_id,omitempty"
-		Name  string          "Name"
+	Id   bson.ObjectId "_id,omitempty"
+	Name string        "Name"
 }
-var Bob = Person{"","Bob"}
-var Jean = Person{"","Jean"}
+
+var Bob = Person{"", "Bob"}
+var Jean = Person{"", "Jean"}
 var dial = &mgo.DialInfo{
-			Addrs: []string{config.MongoHost + ":" + config.MongoPort,},
-			ReplicaSetName: config.MongoReplicaSetName,
-			Username: config.MongoRoot,
-			Password: config.MongoRootPassword,
-						}
-var session, err= mgo.DialWithInfo(dial)
+	Addrs:          []string{config.MongoHost + ":" + config.MongoPort},
+	ReplicaSetName: config.MongoReplicaSetName,
+	Username:       config.MongoRoot,
+	Password:       config.MongoRootPassword,
+}
+var session, err = mgo.DialWithInfo(dial)
 var db = session.DB(randDb)
 var col = db.C(randCol)
 
-var _ = Describe("access to data base", func(){
+var _ = Describe("access to data base", func() {
 
-	Context("When an using a Mongodb intance", func(){
+	Context("When an using a Mongodb intance", func() {
 
-		Context("When creating a session", func(){
+		Context("When creating a session", func() {
 
-			It("should be able to create a session as root", func(){
+			It("should be able to create a session as root", func() {
 				_, err := mgo.DialWithInfo(dial)
 				Expect(err).NotTo(HaveOccurred())
 			})
 		})
 
-		Context("When creating a session as root user", func(){
-			BeforeEach(func(){
-				session, err= mgo.DialWithInfo(dial)
+		Context("When creating a session as root user", func() {
+			BeforeEach(func() {
+				session, err = mgo.DialWithInfo(dial)
 				err = db.UpsertUser(&user)
 			})
-			AfterEach(func(){
+			AfterEach(func() {
 				session.LogoutAll()
 				db.RemoveUser(user.Username)
 				session.Close()
 			})
-			It("should successfully create an admin user for a given database", func(){
+			It("should successfully create an admin user for a given database", func() {
 				Expect(err).NotTo(HaveOccurred())
 			})
-			Context("When logging to a database as an admin user", func(){
-				BeforeEach(func(){
-					session, err= mgo.DialWithInfo(dial)
+			Context("When logging to a database as an admin user", func() {
+				BeforeEach(func() {
+					session, err = mgo.DialWithInfo(dial)
 					err = db.UpsertUser(&user)
 				})
-				AfterEach(func(){
+				AfterEach(func() {
 					session.LogoutAll()
 					db.RemoveUser(user.Username)
 					session.Close()
 				})
-				It("should login successfully as this user", func(){
+				It("should login successfully as this user", func() {
 					err = db.Login(user.Username, user.Password)
 					Expect(err).NotTo(HaveOccurred())
 				})
 			})
 		})
-		Context("When logging to a database as an admin user", func(){
+		Context("When logging to a database as an admin user", func() {
 
-			Context("When a document is inserted in the database ", func(){
+			Context("When a document is inserted in the database ", func() {
 
-				BeforeEach(func(){
-					session, err= mgo.DialWithInfo(dial)
+				BeforeEach(func() {
+					session, err = mgo.DialWithInfo(dial)
 					db.UpsertUser(&user)
-					err= col.Insert(Bob)
+					err = col.Insert(Bob)
 					db.Login(user.Username, user.Password)
 				})
 
-				AfterEach(func(){
-					col.Remove(bson.M{"Name":"Bob"})
+				AfterEach(func() {
+					col.Remove(bson.M{"Name": "Bob"})
 					col.DropCollection()
 					session.LogoutAll()
 					db.RemoveUser(user.Username)
 					session.Close()
 				})
 
-				It("should insert a document and retrieve it", func(){
-					search:= col.Find(bson.M{"Name":"Bob"})
+				It("should insert a document and retrieve it", func() {
+					search := col.Find(bson.M{"Name": "Bob"})
 					Expect(search.Count()).To(Equal(1))
 				})
 
-				It("should be able to update an existing document", func(){
+				It("should be able to update an existing document", func() {
 					col.Update(bson.M{"Name": "Bob"}, bson.M{"$set": bson.M{"Name": "Pierre"}})
-					search:= col.Find(bson.M{"Name":"Pierre"})
+					search := col.Find(bson.M{"Name": "Pierre"})
 					Expect(search.Count()).To(Equal(1))
 				})
 
-				It("should be able to delete an existing document", func(){
-					err = col.Remove(bson.M{"Name":"Bob"})
+				It("should be able to delete an existing document", func() {
+					err = col.Remove(bson.M{"Name": "Bob"})
 					Expect(err).NotTo(HaveOccurred())
-					search:= col.Find(bson.M{"Name":"Bob"})
+					search := col.Find(bson.M{"Name": "Bob"})
 					Expect(search.Count()).To(Equal(0))
 				})
 			})
